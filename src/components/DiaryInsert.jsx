ea7a99411ff295d1f3a0b850cd3db35b9d009952
IvoryGolds2 <sakuravely@naver.com>
2023-06-08 19:01:57 +0900

    만들어보쟝

---
+++ src/components/DiaryInsert.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DiaryInsert(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default DiaryInsert;
---
+++ src/components/DiaryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DiaryList(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default DiaryList;
